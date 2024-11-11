bfa4a477cdb479a7bec67d6b08df57ce6102a50f
Dany246 <145447252+Dany246@users.noreply.github.com>
2024-11-11 11:47:03 +0100

    hehe

--- halloween/src/Components/Navbar.jsx
+++ halloween/src/Components/Navbar.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 export default function Navbar() {
+
+    const [activePath, setActivePath] = useState(window.location.pathname);
+
+    const handleClick = (e) => {
+        setActivePath(e.target.pathname);
+    }
+
     return (
-        <nav >
-            <ul >
-                <li><a href="/filmek">Halloween Franchise</a></li>
-                <li><a href="/">Főoldal</a></li>
-                <li><a href="/unnep">Halloween ünnep</a></li>
+        <nav>
+            <ul>
+                <li><a className={activePath === "/filmek" ? "flinks" : "nlinks"} onClick={handleClick} href="/filmek">Halloween Franchise</a></li>
+                <li><a className={activePath === "/" ? "flinks" : "nlinks"} onClick={handleClick} href="/">Fooldal</a></li> 
+                <li><a className={activePath === "/unnep" ? "flinks" : "nlinks"} onClick={handleClick} href="/unnep">Halloween unnep</a></li>
             </ul>
         </nav>
     );
