b80a2ebd2581d42d3580db3fea4ba529ff1eb1f7
Dany246 <145447252+Dany246@users.noreply.github.com>
2024-11-11 12:19:35 +0100

    anyam

--- halloween/src/Components/Navbar.jsx
+++ halloween/src/Components/Navbar.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import { Facebook } from 'lucide-react';
+import { Instagram } from 'lucide-react';
+import { Github } from 'lucide-react';
+import { Mail } from 'lucide-react';
 export default function Navbar() {
 
     const [activePath, setActivePath] = useState(window.location.pathname);
@@ -8,12 +12,22 @@ export default function Navbar() {
     }
 
     return (
+        <>
         <nav>
             <ul>
                 <li><a className={activePath === "/filmek" ? "flinks" : "nlinks"} onClick={handleClick} href="/filmek">Halloween Franchise</a></li>
                 <li><a className={activePath === "/" ? "flinks" : "nlinks"} onClick={handleClick} href="/">Fooldal</a></li> 
                 <li><a className={activePath === "/unnep" ? "flinks" : "nlinks"} onClick={handleClick} href="/unnep">Halloween unnep</a></li>
             </ul>
+            <ul className="icons">
+                <a href=""><Facebook color="red" size={35}/></a>
+                <a href=""><Instagram color="red" size={35}/></a>
+                <a href=""><Github color="red" size={35}/></a>
+                <a href=""><Mail color="red" size={35}/></a><a href=""></a>
+            </ul>
+           
         </nav>
+        </>
+     
     );
 }
--- halloween/src/Pages/Fooldal.jsx
+++ halloween/src/Pages/Fooldal.jsx
@@ -3,7 +3,13 @@
 const Fooldal = () => {
     return (
         <>
-            <h1>Fooldal</h1>
+            <h1 className="F1">Fooldal</h1>
+            <h2 className="F2">Készítők: <br />
+            <h2 className="F2">Tóth Bence és Éri Tibor Dániel</h2></h2>
+        <p className="ksz">Szia Dia! <br /> Ezt az oldalt Tóth Bence és Éri Tibor Dániel készíti neked hatalmas szeretettel. <br /> A Halloweenről a filmes franchise és maga az ünnep jutott eszünkbe szóval z oldalon ennek a két témának a feldolgozását olvashatod. <br />
+            Szia
+        </p>
+        
         </>
     )
 }
