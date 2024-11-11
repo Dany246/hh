b013984e75b0e8071fc9932ffb120126fa70c91a
DanielEri04 <Daniel184504@gmail.com>
2024-11-11 23:53:20 +0100

    na

--- halloween/src/Pages/Fooldal.jsx
+++ halloween/src/Pages/Fooldal.jsx
@@ -3,7 +3,7 @@
 const Fooldal = () => {
     return (
         <>
-            <h1 className="F1">Fooldal</h1>
+            <h1 className="F1">Foldal</h1>
             <h2 className="F2">Készítők: <br /></h2>
             <h2 className="F2">Tóth Bence és Éri Tibor Dániel</h2>
         <p className="ksz">Szia Dia<br /> Ezt az oldalt Toth Bence es Eri Tibor Daniel kesziti neked hatalmas szeretettel. <br /> A Halloweenrol a filmes franchise es maga az unnep jutott eszunkbe szoval az oldalon ennek a ket temanak a feldolgozasat olvashatod. <br />
