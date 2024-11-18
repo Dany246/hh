0d3ca9837e8d7e67337e4993e4254cd8a1b308f0
niezz01 <145447146+niezz01@users.noreply.github.com>
2024-11-18 12:20:16 +0100

    kesz

--- halloween/src/Pages/Unnep.jsx
+++ halloween/src/Pages/Unnep.jsx
@@ -4,7 +4,8 @@ const Unnep = () => {
     return (
         <>
             <h1 className="F1">Az unnep</h1>
-                <h2 className="F2">Halloween eredete</h2>
+            <img className="pic1" src="../public/unnepek/soktok.jpg" alt="" />
+            <h2 className="F2">Halloween eredete</h2>
             <div className="ubk">
                 <p className="T1">A Halloweenre a legtöbb ember amerikai ünnepként gondol, pedig nem Amerikából származik és az eredete egész régi időkre nyúlik vissza.</p>
                 <p className="T1">A kelták egyik legszentebb ünnepe a Samhain volt, ami az újév, valamit a sötét és hideg évszak kezdetét is jelentette. A kelta hit szerint az újév előestéjén, vagyis október 31-én a világ rendje felborul, fellibbennek az evilág és a túlvilág közötti határok, és az emberfeletti lények, valamint a halottak szabadon járhatnak köztünk. Számos szertartás kapcsolódott ehhez az ünnephez, amelyeknek a célja az volt, hogy távol tartsák a szellemeket.</p>
@@ -15,11 +16,22 @@ const Unnep = () => {
             <div className="ubk">
                 <p className="T1">A Halloween szó mindenszentek előestéjét jelenti, ami az "All Hallows Eve" kifejezés rövidítésével alakult ki. Mindenszentek november 1-én van. Halloween pedig október 31-én.</p>
             </div>
+            <img className="pic2" src="../public/unnepek/halloween1.jpg" alt="" />
             <h2 className="F2">Hogyan ünneplik a Halloweent?</h2>
             <div className="ubk">
                 <p className="T1">Az emberek maszkokat vesznek és jelmezbe öltöznek. A tradicionális Halloween jelmezek természetfeletti figurák, például vámpírok, szörnyek, szellemek, csontvázok, boszorkányok és ordögök alapján készülnek, de manapság népszerűek a szuperhős és hercegnő jelmezek is.</p>
                 <p className="T1">A házakat feldíszítik halloweeni dekorációval, aminek fontos kelléke a JackO'Lantern, azaz a faragott töklámpás. A töklámpás érdekessége, hogy az ír bevándorlók általában karórépából készítettek faragott lámpát. Amikor ez a szokás megérkezett az USA-ba, ahol a tök őshonos, a karórépát felváltotta a tök, mivel a tökből sokkal könnyebben lehetett, látványosabb lámpákat faragni. A farmerek már direkt olyan fajtákat nemesítenek, amik a legideálisabbak a tökfaragáshoz.</p>
-                <p className="T1">A "Csokit vagy csalunk" a tévének köszönhetően talán az egyik legismertebb Halloween szokás. Ez elsősorban a gyerekek programja, mikor jelmezbe öltözve bekopogtatnak a házakba ezzel a felkiáltással és csokit gyűjtenek. A "Csokit vagy csalunk!" </p>
+                <p className="T1">A "Csokit vagy csalunk" a tévének köszönhetően talán az egyik legismertebb Halloween szokás. Ez elsősorban a gyerekek programja, mikor jelmezbe öltözve bekopogtatnak a házakba ezzel a felkiáltással és csokit gyűjtenek.</p>
+            </div>
+            <h2 className="F2">Halloween Magyarországon</h2>
+            <div className="ubk">
+                <p className="T1">A halloweeni szokások Magyarországon a 2000-es évek elején kezdtek elterjedni. Egyre többen szerveztek halloweeni bulikat, készitettek töklámpást, öltöztek ijesztő jelmezekbe, készítettek sütőtökos pitét ezen a napon, és Halloween tematikájú rendezvényeket is elkezdtek szervezni.</p>
+            </div>
+            <img className="pic3" src="../public/unnepek/horseman.jpg" alt="" />
+            <div className="ubk">
+                <p className="T1">A fej nélküli lovas világszerte ismert egy mitológiai lény, aki a középkor óta szerepel világszerte a folklórban. Az alakot hagyományosan lóháton ábrázolják, akinek hiányzik a feje.
+                Washington Irving 1820-as novelláját – amelyben feltűnt az élőket rémisztgető fej nélküli lovas – az amerikai folklór egyik korai példájának tekintik. Ám a szellemalak története már a középkor óta rémisztgeti a babonás emberek fantáziáját.
+                A legendától függően a lovas vagy hordja a fejét, vagy teljesen hiányzik neki, de folyton-folyvástkeresi. Az írországi dullahan démon általában lovon ül, és a fejét a hóna alatt hordja a Sir Gawain és a Zöld Lovag című műben. A „The Legend of Sleepy Hollow” című novella, amelyet 1820-ban írt az amerikai Washington Irving, sok irodalmi műben és filmben is megjelenik, például az 1949-es Disney-filmben.</p>
             </div>
         </>
     )
